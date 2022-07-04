c1479d0b559ff563a54fa458700ad5945dcf28a5
AndersonFlorencioConforto <afcaconforto@gmail.com>
2022-07-04 17:13:22 -0300

    validação de salvar um curso para quem estiver autenticado

--- src/main/java/com/ead/course/validation/CourseValidator.java
+++ src/main/java/com/ead/course/validation/CourseValidator.java
@@ -1,5 +1,6 @@
 package com.ead.course.validation;
 
+import com.ead.course.configs.security.AuthenticationCurrentUserService;
 import com.ead.course.dtos.CourseDTO;
 import com.ead.course.models.UserModel;
 import com.ead.course.models.enums.UserType;
@@ -8,6 +9,7 @@ import com.ead.course.services.UserService;
 import com.ead.course.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
@@ -22,6 +24,8 @@ public class CourseValidator implements Validator {
     private Validator validator;
     @Autowired
     private UserService userService;
+    @Autowired
+    private AuthenticationCurrentUserService authenticationCurrentUserService;
 
     @Override
     public boolean supports(Class<?> clazz) {
@@ -38,9 +42,13 @@ public class CourseValidator implements Validator {
     }
 
     private void validateUserInstructor(UUID userInstructor, Errors errors) {
-        UserModel userModel = userService.findById(userInstructor);
-        if (userModel.getUserType().equals(UserType.STUDENT.toString())) {
-            errors.rejectValue("userInstructor", "UserInstructorError", "User must be INSTRUCTOR or ADMIN");
+        UUID userId = authenticationCurrentUserService.getCurrentUser().getUserId();
+        if (userId.equals(userInstructor)) {
+            UserModel userModel = userService.findById(userInstructor);
+            if (userModel.getUserType().equals(UserType.STUDENT.toString())) {
+                errors.rejectValue("userInstructor", "UserInstructorError", "User must be INSTRUCTOR or ADMIN");
+            }
         }
+        throw new AccessDeniedException("Forbbiden");
     }
 }
